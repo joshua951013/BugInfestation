f8065d0010f494b413804724b302ba1670bd259e
Joshua <joshua951013@naver.com>
2018-03-30 16:01:39 +0900

    Brought the initial state varibles into the main method instead, and imported and instantiated Scanner

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -2,19 +2,26 @@ package edu.handong.csee.java.lab06;
 import java.util.Scanner; // 
 public class BugCalcul {
 	
-	double GrowthRate =0.95;
-	double One_Bug_Vol=0.002;
-	int StartPopul;
-	int HouseVol;
-	int CountWeeks;
-	int population;
-	int TotalBugVol;
-	int NewBugs;
-	int NewBugVol;
 	
-	
-
 	public static void main(String[] args) {  // public method main is made
+	
+		double GrowthRate =0.95;
+		double One_Bug_Vol=0.002;
+		int StartPopul;
+		int HouseVol;
+		int CountWeeks;
+		int population;
+		int TotalBugVol;
+		int NewBugs;
+		int NewBugVol;
+		
+		Scanner keyboard = new Scanner(System.in); //instantiating my keyboard
+		System.out.print("Enter the total volume of your house in cubic feet:");
+		HouseVol = keyboard.nextInt();
+		System.out.print("Enter the estimated number of roaches in your house:");
+		StartPopul = keyboard.nextInt();
+		
+		
 		
 		
 		

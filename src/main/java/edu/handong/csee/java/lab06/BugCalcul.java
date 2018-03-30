001609cd67893a3860928f2f6ff5537388d3db3d
Joshua <joshua951013@naver.com>
2018-03-30 18:56:23 +0900

    first trial

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -1,6 +1,8 @@
 package edu.handong.csee.java.lab06;
 import java.util.Scanner; // 
+
 public class BugCalcul {
+	
 	
 	
 	public static void main(String[] args) {  // public method main is made
@@ -9,17 +11,34 @@ public class BugCalcul {
 		double One_Bug_Vol=0.002;
 		int StartPopul;
 		int HouseVol;
-		int CountWeeks;
-		int population;
-		int TotalBugVol;
-		int NewBugs;
-		int NewBugVol;
+		int CountWeeks=0;
+		double population= 0;
+		double TotalBugVol=0;
+		double NewBugs;
+		double NewBugVol;
 		
 		Scanner keyboard = new Scanner(System.in); //instantiating my keyboard
-		System.out.print("Enter the total volume of your house in cubic feet:");
+		System.out.println("Enter the total volume of your house in cubic feet:");
 		HouseVol = keyboard.nextInt();
-		System.out.print("Enter the estimated number of roaches in your house:");
+		System.out.println("Enter the estimated number of roaches in your house:");
 		StartPopul = keyboard.nextInt();
+		
+
+        while (TotalBugVol < HouseVol)
+        {
+        	NewBugs = StartPopul * GrowthRate;
+        	
+            NewBugVol = NewBugs * One_Bug_Vol;
+
+            population = population + NewBugs;
+
+            TotalBugVol = TotalBugVol + NewBugVol;
+
+            CountWeeks = CountWeeks + 1;
+        }
+
+        System.out.println("Starting with a roach population of "+StartPopul+" and a house with a volume of "+HouseVol+" cubic feet, after "+CountWeeks+" weeks, the house will be filled with"+population+"roaches. They will fill a volume of "+TotalBugVol+" cubic feet. Better call Debugging Experts Inc.");
+
 		
 		
 		

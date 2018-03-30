77141ae635bdda5b6d183d343f478e11226cf748
Joshua <joshua951013@naver.com>
2018-03-30 15:38:50 +0900

    initial Var added

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -1,7 +1,23 @@
 package edu.handong.csee.java.lab06;
 import java.util.Scanner; // 
 public class BugCalcul {
+	
+	double GrowthRate =0.95;
+	double One_Bug_Vol=0.002;
+	int StartPopul;
+	int HouseVol;
+	int CountWeeks;
+	int population;
+	int TotalBugVol;
+	int NewBugs;
+	int NewBugVol;
+	
+	
+
 	public static void main(String[] args) {  // public method main is made
+		
+		
+		
 		
 	}
 	

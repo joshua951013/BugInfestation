f3adb73f8febe4b260d923c4a7c61833c8e50b21
Joshua <joshua951013@naver.com>
2018-03-30 15:05:54 +0900

    main method is added

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -1,5 +1,9 @@
 package edu.handong.csee.java.lab06;
-
+import java.util.Scanner; // 
 public class BugCalcul {
-
+	public static void main(String[] args) {  // public method main is made
+		
+	}
+	
+	
 }

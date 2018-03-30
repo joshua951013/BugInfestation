36fa4e72742f64a3b2c19c84805ae2a122441b45
Joshua <joshua951013@naver.com>
2018-03-30 20:53:03 +0900

    Third trial with the proper lining for the OUTPUT

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -11,9 +11,9 @@ public class BugCalcul {
         double houseVolume;
     	int startPopulation;
     	Scanner keyboard = new Scanner (System.in);
-        System.out.print ("Enter the total volume of your house in cubic feet: ");
+        System.out.println ("Enter the total volume of your house in cubic feet: ");
         houseVolume = keyboard.nextDouble ();
-        System.out.print ("Enter the estimated number of roaches in your house: ");
+        System.out.println ("Enter the estimated number of roaches in your house: ");
         startPopulation = keyboard.nextInt ();
         
         double population = startPopulation;
@@ -33,11 +33,11 @@ public class BugCalcul {
             
             countWeeks = countWeeks + 1;
         }
-        System.out.print ("Starting with a roach population of " +startPopulation);
-        System.out.print ("and a house with a volume of " + houseVolume +" cubic feet,");
-        System.out.print ("after " + countWeeks + " weeks,");
-        System.out.print ("the house will be filled with " + population + " roaches.");
-        System.out.print ("They will fill a volume of " + totalBugVolume + " cubic feet.");
-        System.out.print ("Better call Debugging Experts Inc.");
+        System.out.println ("Starting with a roach population of " +startPopulation);
+        System.out.println ("and a house with a volume of " + houseVolume +" cubic feet,");
+        System.out.println ("after " + countWeeks + " weeks,");
+        System.out.println ("the house will be filled with " + population + " roaches.");
+        System.out.println ("They will fill a volume of " + totalBugVolume + " cubic feet.");
+        System.out.println ("Better call Debugging Experts Inc.");
     }
 }

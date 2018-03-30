4b8ae1ff106c101541a34a707cd2a2fda0e14a4d
Joshua <joshua951013@naver.com>
2018-03-30 20:50:40 +0900

    Second trial

--- src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
+++ src/main/java/edu/handong/csee/java/lab06/BugCalcul.java
@@ -1,51 +1,43 @@
 package edu.handong.csee.java.lab06;
-import java.util.Scanner; // 
+import java.util.Scanner;  
 
 public class BugCalcul {
 	
-	
-	
-	public static void main(String[] args) {  // public method main is made
-	
-		double GrowthRate =0.95;
-		double One_Bug_Vol=0.002;
-		int StartPopul;
-		int HouseVol;
-		int CountWeeks=0;
-		double population= 0;
-		double TotalBugVol=0;
-		double NewBugs;
-		double NewBugVol;
-		
-		Scanner keyboard = new Scanner(System.in); //instantiating my keyboard
-		System.out.println("Enter the total volume of your house in cubic feet:");
-		HouseVol = keyboard.nextInt();
-		System.out.println("Enter the estimated number of roaches in your house:");
-		StartPopul = keyboard.nextInt();
-		
-
-        while (TotalBugVol < HouseVol)
+	static double GROWTH_RATE = 0.95; 
+    static double ONE_BUG_VOLUME = 0.002; 
+    
+    public static void main (String [] args){
+ 
+        double houseVolume;
+    	int startPopulation;
+    	Scanner keyboard = new Scanner (System.in);
+        System.out.print ("Enter the total volume of your house in cubic feet: ");
+        houseVolume = keyboard.nextDouble ();
+        System.out.print ("Enter the estimated number of roaches in your house: ");
+        startPopulation = keyboard.nextInt ();
+        
+        double population = startPopulation;
+        double totalBugVolume = population * ONE_BUG_VOLUME;
+        double newBugs, newBugVolume;
+        int countWeeks = 0;
+        
+        while (totalBugVolume < houseVolume)
         {
-        	NewBugs = StartPopul * GrowthRate;
-        	
-            NewBugVol = NewBugs * One_Bug_Vol;
-
-            population = population + NewBugs;
-
-            TotalBugVol = TotalBugVol + NewBugVol;
-
-            CountWeeks = CountWeeks + 1;
+            newBugs = population * GROWTH_RATE;
+            
+            newBugVolume = newBugs * ONE_BUG_VOLUME;
+            
+            population = population + newBugs;
+            
+            totalBugVolume = totalBugVolume + newBugVolume;
+            
+            countWeeks = countWeeks + 1;
         }
-
-        System.out.println("Starting with a roach population of "+StartPopul+" and a house with a volume of "+HouseVol+" cubic feet, after "+CountWeeks+" weeks, the house will be filled with"+population+"roaches. They will fill a volume of "+TotalBugVol+" cubic feet. Better call Debugging Experts Inc.");
-
-		
-		
-		
-		
-		
-		
-	}
-	
-	
+        System.out.print ("Starting with a roach population of " +startPopulation);
+        System.out.print ("and a house with a volume of " + houseVolume +" cubic feet,");
+        System.out.print ("after " + countWeeks + " weeks,");
+        System.out.print ("the house will be filled with " + population + " roaches.");
+        System.out.print ("They will fill a volume of " + totalBugVolume + " cubic feet.");
+        System.out.print ("Better call Debugging Experts Inc.");
+    }
 }
